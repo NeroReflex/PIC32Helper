02321c175e99dfb5fc9ad81692372a814b63ad15
Benato Denis <benato.denis96@gmail.com>
2018-07-14 19:42:28 +0200

    Removed useless files

--- FreeRTOS/build/default/production/src/port_asm.o.d
+++
@@ -1,8 +0,0 @@
-build/default/production/src/port_asm.o:  \
-src/port_asm.S  \
-src/ISR_Support.h  \
- src/FreeRTOSConfig.h 
-build/default/production/src/port_asm.o:  \
- src/FreeRTOSConfig.h  \
-src/ISR_Support.h  \
- src/port_asm.S  \
